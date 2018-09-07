0d99514f8d8dadd3cd3b9adf0eb236853f0371ab
1002237 <wonwoo@sk.com>
2018-09-07 13:52:16 +0900

    htmlstring to attribute string

--- extension/Extensions.swift
+++ extension/Extensions.swift
@@ -30,3 +30,29 @@ extension UIButton{
         self.frame = CGRect(x: self.frame.origin.x, y: self.frame.origin.y, width: buttonWidth, height: self.frame.size.height)
     }
 }
+
+
+
+
+/*  html string을 attribute string으로 변환
+ *  eg.
+ *  let attributeString = NSMutableAttributedString(html: htmlString)
+ *  attributeString.addAttributes([NSAttributedStringKey.font:UIFont.adjustedOcbFont(withSize: 14.0, withBaseWidth: 375.0)], range: NSMakeRange(0, attributeString.length))
+ *  label.attributedText = htmlString
+ */
+
+extension NSMutableAttributedString{
+    internal convenience init?(html: String) {
+        guard let data = html.data(using: String.Encoding.utf16, allowLossyConversion: false) else {
+            return nil
+        }
+        
+        guard let attributedString = try?  NSMutableAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding: String.Encoding.utf8.rawValue], documentAttributes: nil) else {
+            return nil
+        }
+        
+        self.init(attributedString: attributedString)
+    }
+}
+
+
