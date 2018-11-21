f961318f194d7b35b7f7854b6e3d9585aa7b5e85
wonwoo518 <wonwoo518@gmail.com>
2018-11-21 18:42:32 +0900

    Add files via upload

---
+++ DynamicText/DynamicText/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DynamicText
+//
+//  Created by 1002237 on 21/11/2018.
+//  Copyright © 2018 1002237. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
