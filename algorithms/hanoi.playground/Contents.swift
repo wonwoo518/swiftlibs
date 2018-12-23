b4c3e2c0fbcf44fd5d09815dab7c0b5803ba7680
wonwoo <wonwoo518@gmail.com>
2018-12-23 15:00:11 +0900

    -

--- algorithms/hanoi.playground/Contents.swift
+++ algorithms/hanoi.playground/Contents.swift
@@ -1,15 +1,23 @@
 import UIKit
 
+
+//하노이 탑의 포인트
+//n개의 디스크를 목적지에 옮기기 위해선 반드시 아래와 같은 순서로 동작
+//1. 꼭대기부터 n-1까진 경유지(aux)로 이동 
+//2. 맨 아래 디스크는 목적지(to) 로 이동
+//3. 꼭대기부터 n-1까지 경유지(aux)에서 목적지(to)로 이동
+
 func hanoi(diskNum n:Int,from: Character,to: Character,by:Character) {
     if n == 1 { // 종료조건 (n = 1 이 문제 범위에서 가장 작은 단위)
         print(" 원반 1 을 \(from) 에서 \(to) 로 이동.")
     }else {
-        hanoi(diskNum: n-1, from: from, to: by, by: to) // a 축에서 c 축으로 원판들 이동시키자!
-        print(" 원반 \(n) 을 \(from) 에서 \(to) 로 이동.") // 위에 아무것도 없으니 젤 무거운 놈 b 축으로 이동시키자!
-        hanoi(diskNum: n-1, from:by , to: to, by: from) // c 에 있던 원판들 b 축에 다시 쌓자!
+        hanoi(diskNum: n-1, from: from, to: by, by: to) // 알고리즘 #1
+        print(" 원반 \(n) 을 \(from) 에서 \(to) 로 이동.")   // 알고리즘 #2
+        hanoi(diskNum: n-1, from:by , to: to, by: from) // 알고리즘 #3
     }
 }
-//hanoi(diskNum: 4, from: "a", to: "c", by: "b")
+
+hanoi(diskNum: 2, from: "a", to: "c", by: "b")
 
 
 class HanoiV2{
@@ -62,7 +70,7 @@ class HanoiV2{
 }
 
 
-var hanoiV2 = HanoiV2(arr: [4,3,2,1])
-hanoiV2.run()
+//var hanoiV2 = HanoiV2(arr: [4,3,2,1])
+//hanoiV2.run()
 
 
