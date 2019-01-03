ca6aa581d9589fd3b6c80b1df72578858847d892
wonwoo <wonwoo518@gmail.com>
2019-01-03 09:04:20 +0900

    -

--- algorithms/bst.playground/Contents.swift
+++ algorithms/bst.playground/Contents.swift
@@ -1,17 +1,49 @@
 import UIKit
 
 class BinarySearchTree<T:Comparable>{
-    struct Node{
+    class Node<T>{
         var val:T
-        var next:Node?
-        var prev:Node?
+        var left:Node? = nil
+        var right:Node? = nil
+        
+        init(val:T) {
+            self.val = val
+        }
+        
     }
 
+    var rootNode:Node<T>? = nil
+
     func insert(insertItem:T){
+        if rootNode == nil{
+            rootNode = Node(val: insertItem)
+            return
+        }
+        
+        var curNode:Node<T>? = nil
+        var parentNode = rootNode
+        
+        while parentNode != nil {
+            
+            if parentNode?.val == insertItem{
+                return
+            }
+            
+            if parentNode?.val > insertItem{
+                parentNode = parentNode?.right
+                continue
+            }
+            
+            if parentNode?.val < insertItem{
+                
+            }
+        }
         
     }
     
+    
     func traverse(searchVal:T){
+        print("")
     }
     
     func showTree(){
