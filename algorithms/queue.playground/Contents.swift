9c4eb161da74fdcbb513c4f72d765c9b7018beff
wonwoolee <wonwoo@sk.com>
2019-01-11 14:18:27 +0900

    -

--- algorithms/queue.playground/Contents.swift
+++ algorithms/queue.playground/Contents.swift
@@ -1,7 +1,7 @@
 // create by wonwoo.lee
 import UIKit
 
-class MyQueueV1<T>{
+struct MyQueueV1<T>{
     private var array:Array<T> = Array<T>()
     func isEmpty() -> Bool {
         return array.isEmpty
@@ -11,11 +11,11 @@ class MyQueueV1<T>{
         return array.count
     }
 
-    func enque(val:T){
+    mutating func enqueue(val:T){
         array.append(val)
     }
     
-    func deque()->T?{
+    mutating func dequeue()->T?{
         if array.isEmpty {
             return nil
         }
@@ -31,20 +31,25 @@ class MyQueueV1<T>{
 }
 //Test Code
 //var q = MyQueueV1<String>()
-//q.enque(val: "1")
-//q.enque(val: "2")
-//q.enque(val: "3")
-//q.enque(val: "4")
+//q.enqueue(val: "1")
+//q.enqueue(val: "2")
+//q.enqueue(val: "3")
+//q.enqueue(val: "4")
 //q.qprint()
-//q.deque()
+//q.dequeue()
 //q.qprint()
 
 
 //array copy issue customize
 //array first pop시 뒤의 원소를 모두 앞으로 한칸씩 옮기는 작업 효율화.
-class MyQueueV2<T>{
+struct MyQueueV2<T>{
     private var head:Int = 0
     private var array:Array<T?> = Array<T?>()
+    
+    public init<S: Sequence>(_ elements: S) where S.Iterator.Element == T? {
+        array.append(contentsOf: elements)
+    }
+    
     func isEmpty() -> Bool {
         return array.isEmpty
     }
@@ -53,17 +58,17 @@ class MyQueueV2<T>{
         return array.count
     }
     
-    func enque(val:T){
+    mutating func enqueue(val:T){
         
         array.append(val)
     }
     
-    func deque()->T?{
+    mutating func dequeue()->T?{
         if array.isEmpty {
             return nil
         }
         
-        var popVal = array[head]
+        let popVal = array[head]
         array[head] = nil
         head += 1
         
@@ -72,7 +77,7 @@ class MyQueueV2<T>{
         return popVal
     }
     
-    private func arrayRearrange(){
+    private mutating func arrayRearrange(){
         var wasteRate:CGFloat = 0.0
         wasteRate = array.count < 5 ? 100.0 : CGFloat((head) * 100) / CGFloat(array.count)
         if wasteRate > 50.0{
@@ -80,37 +85,72 @@ class MyQueueV2<T>{
             head = 0
         }
     }
+}
+
+extension MyQueueV2 : CustomStringConvertible, CustomDebugStringConvertible{
+    public var description: String {
+        get{
+            return array.description
+        }
+    }
     
-    func qprint(){
-        print(array)
+    public var debugDescription: String {
+        get{
+            return array.description
+        }
     }
 }
 
-//Test Code
+extension MyQueueV2 : ExpressibleByArrayLiteral{
+    public init(arrayLiteral elements: T...){
+        self.init(elements)
+    }
+}
+
+extension MyQueueV2 : Sequence {
+    public func makeIterator() -> AnyIterator<T?> {
+        return AnyIterator(IndexingIterator(_elements: array))
+    }
+}
+
+////Test Code
 //var q = MyQueueV2<String>()
-//q.enque(val: "하늘")
-//q.enque(val: "땅")
-//q.enque(val: "바람")
-//q.enque(val: "구름")
-//q.enque(val: "비")
-//q.qprint()
-//q.deque()
-//q.qprint()
-//q.deque()
-//q.qprint()
-//q.deque()
-//q.qprint()
-//q.deque()
-//q.qprint()
-//q.enque(val: "1")
-//q.enque(val: "2")
-//q.enque(val: "3")
-//q.enque(val: "4")
-//q.qprint()
-//q.deque()
-//q.qprint()
-//q.deque()
-//q.qprint()
-//q.deque()
-//q.qprint()
+var q = MyQueueV2<String>(["하늘", "땅", "바람", "구름", "비"])
+print(q)
+var q2:MyQueueV2<String> = ["하늘", "땅", "바람", "구름", "비"]
+print(q2)
+
+for el in q2{
+    print(el)
+}
+
+//q.enqueue(val: "하늘")
+//q.enqueue(val: "땅")
+//q.enqueue(val: "바람")
+//q.enqueue(val: "구름")
+//q.enqueue(val: "비")
+//print(q)
+//q.dequeue()
+//print(q)
+//q.dequeue()
+//print(q)
+//q.dequeue()
+//print(q)
+//q.dequeue()
+//print(q)
+//q.enqueue(val: "1")
+//q.enqueue(val: "2")
+//q.enqueue(val: "3")
+//q.enqueue(val: "4")
+//print(q)
+//q.dequeue()
+//print(q)
+//q.dequeue()
+//print(q)
+//q.dequeue()
+//print(q)
+
+
+
+
 
